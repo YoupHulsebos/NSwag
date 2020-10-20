812857e02c345d9bae9cd93a7257797faa89ff86
Youp Hulsebos <youph@outlook.com>
2020-10-20 10:51:46 +0200

    Made my own test

--- src/NSwag.Core.Yaml.Tests/References/YamlReferencesTests.cs
+++ src/NSwag.Core.Yaml.Tests/References/YamlReferencesTests.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using NJsonSchema;
+using NSwag.CodeGeneration.CSharp;
 using Xunit;
 
 namespace NSwag.Core.Yaml.Tests.References
@@ -15,7 +16,6 @@ namespace NSwag.Core.Yaml.Tests.References
         [InlineData("/References/YamlReferencesTest/yaml_contract_with_json_reference.yaml")]
         [InlineData("/References/YamlReferencesTest/yaml_contract_with_yaml_reference.yaml")]
         [InlineData("/References/YamlReferencesTest/json_contract_with_yaml_reference.json")]
-        [InlineData("/References/YamlReferencesTest/yaml_contract_path_reference.yaml")]
         public async Task When_yaml_schema_has_references_it_works(string relativePath)
         {
             //// Arrange
@@ -28,6 +28,32 @@ namespace NSwag.Core.Yaml.Tests.References
             //// Assert
             Assert.Equal(JsonObjectType.Integer, document.Definitions["ContractObject"].Properties["foo"].ActualTypeSchema.Type);
             Assert.Equal(JsonObjectType.Boolean, document.Definitions["ContractObject"].Properties["bar"].ActualTypeSchema.Type);
+        }
+
+        [Theory]
+        [InlineData("/References/YamlReferencesTest/yaml_contract_path_reference.yaml")]
+        public async Task When_yaml_schema_has_references_it_can_generate_a_client(string relativePath)
+        {
+            //// Arrange
+            var path = GetTestDirectory() + relativePath;
+
+            //// Act
+            var document = await OpenApiYamlDocument.FromFileAsync(path);
+
+            var settings = new CSharpClientGeneratorSettings
+            {
+                ClassName = "TestClass",
+                CSharpGeneratorSettings =
+                {
+                    Namespace = "TestNamespace"
+                }
+            };
+
+            var generator = new CSharpClientGenerator(document, settings);
+            var code = generator.GenerateFile();
+
+            //// Assert
+            Assert.True(!string.IsNullOrEmpty(code));
         }
 
         private string GetTestDirectory()
